ba4c304087491195ed73447d2768f62b73fbf316
Ashwin <devcodeash@gmail.com>
2020-11-14 09:39:45 +0000

    Production api

--- src/admin/index.js
+++ src/admin/index.js
@@ -3,10 +3,14 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import { MovieList, MovieIcon } from './movies';
+const API =
+  process.env.NODE_ENV === 'production'
+    ? 'https://ash-movie-db.herokuapp.com/api'
+    : 'http://localhost:4000/api';
 
 function AdminApp() {
   return (
-    <Admin dataProvider={jsonServerProvider('http://localhost:4000/api')}>
+    <Admin dataProvider={jsonServerProvider(API)}>
       <Resource name="movies" list={MovieList} icon={MovieIcon} />
     </Admin>
   );
